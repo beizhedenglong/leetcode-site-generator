59ce87f4baf3cb45d9003a8f4957329f6f00d462
Victor Wang <wang_shaojie@icloud.com>
2019-04-06 16:11:18 +0800

    download as json file

--- src/download.js
+++ src/download.js
@@ -1,42 +1,12 @@
 const fs = require('fs');
-const path = require('path');
 const ora = require('ora');
 const { getAllACQuestions, getAcCode } = require('./leetcode');
 
-const toDoc = ({
-  title,
-  titleSlug,
-  lang,
-  code,
-  content,
-}) => {
-  const str = (
-    // eslint-disable-next-line
-`---
-id: ${titleSlug}
-title: ${title}
-sidebar_label: ${title}
----
-## Description
-${content}
-
-## Code
-\`\`\`${lang}
-${code}
-\`\`\``
-  );
-  return str;
-};
-
 const download = async () => {
-  // NOTE
-  const codeDir = path.join('docs');
+  const problemsPath = 'problems.json';
+  const problems = [];
   const questions = await getAllACQuestions();
   const spinner = ora('Downloading accepted code.').start();
-  const isCodeDirExist = fs.existsSync(codeDir);
-  if (!isCodeDirExist) {
-    fs.mkdirSync(codeDir);
-  }
   const aux = async (xs = []) => {
     if (xs.length === 0) {
       return;
@@ -49,14 +19,12 @@ const download = async () => {
     current.code = code;
     current.lang = lang;
     spinner.text = `${questions.length - xs.length}/${questions.length}: [${current.title}] has downloaded.`;
-    fs.writeFile(path.join(codeDir, `${current.titleSlug}.md`), toDoc(current), (err) => {
-      if (err) {
-        console.error(`${current.titleSlug} write error`);
-      }
-    });
-    aux(xs);
+    problems.push(current);
+    fs.writeFileSync(problemsPath, JSON.stringify(problems));
+    await aux(xs);
   };
-  aux([...questions]);
+  await aux([...questions]);
+  spinner.stop();
 };
 
 module.exports = download;
